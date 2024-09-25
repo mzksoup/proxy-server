65073c43020a8813f8aa99e7ce20e500bfc9d0cb
mzksoup <sei_r+git@addor.jp>
2024-09-25 21:50:10 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
+const url = require("url");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10000,
+});
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.send("This is my proxy server");
+});
+app.use("/weather-data", (req, res, next) => {
+  const city = url.parse(req.url).query;
+  createProxyMiddleware({
+    target: `${process.env.BASE_API_URL_WEATHERAPI}${city}&aqi=no`,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^"/weather-data`]: "",
+    },
+  })(req, res, next);
+});
+
+app.use("/corona-tracker-country-data", (req, res, next) => {
+  const city = url.parse(req.url).query;
+  createProxyMiddleware({
+    target: `${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/corona-tracker-country-data`]: "",
+    },
+  })(req, res, next);
+});
+
+app.use("/corona-tracker-world-data", (req, res, next) => {
+  createProxyMiddleware({
+    target: process.env.BASE_API_URL_CORONA_WORLD,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/corona-tracker-world-data`]: "",
+    },
+  })(req, res, next);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
